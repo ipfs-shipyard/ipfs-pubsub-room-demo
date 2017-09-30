e4de87537957ecc3bd279eeab829cd88c0a4939b
Dmitriy Ryajov <dryajov@gmail.com>
2017-09-30 02:10:04 -0600

    fix: move logic into `ready` event

--- src/app.js
+++ src/app.js
@@ -11,22 +11,21 @@ const ipfs = new IPFS({
 ipfs.once('ready', () => ipfs.id((err, info) => {
   if (err) { throw err }
   console.log('IPFS node ready with address ' + info.id)
+
+  const room = Room(ipfs, 'ipfs-pubsub-demo')
+
+  room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
+  room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
+
+  // send and receive messages
+
+  room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
+  room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
+
+  // broadcast message every 2 seconds
+
+  setInterval(() => room.broadcast('hey everyone!'), 2000)
 }))
-
-const room = Room(ipfs, 'ipfs-pubsub-demo')
-
-room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
-room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
-
-// send and receive messages
-
-room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
-room.on('message', (message) =>
-  console.log('got message from ' + message.from + ': ' + message.data.toString()))
-
-// broadcast message every 2 seconds
-
-setInterval(() => room.broadcast('hey everyone!'), 2000)
 
 function repo () {
   return 'ipfs/pubsub-demo/' + Math.random()
