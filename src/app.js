582a4c81de3370616cebf6ddc117efa3f58388b2
Pedro Teixeira <i@pgte.me>
2017-12-12 11:01:13 +0000

    upgraded to latest js-ipfs and ipfs-pubsub-room

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ const ipfs = new IPFS({
   repo: repo(),
   EXPERIMENTAL: {
     pubsub: true
+  },
+  config: {
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
   }
 })
 
